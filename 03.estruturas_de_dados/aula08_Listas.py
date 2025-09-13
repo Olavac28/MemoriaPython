79ef5cdfaf033144b35dd6dd006e1c5d2516b61b
Hans <hans28h@gmail.com>
2025-09-13 14:13:28 -0300

    estruturas de dados

---
+++ 03.estruturas_de_dados/aula08_Listas.py
@@ -0,0 +1,38 @@
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+lista = l1 + l2 #junta as listas
+print(lista)
+
+print(lista[0]) #mostra o primeiro indice
+print(lista[-1]) #mostra o ultimo indice
+
+print(len(lista)) #mostra o tamanho da lista
+print(sorted(lista, reverse=True)) #mostra a lista invertida
+print(sum(lista)) #soma toda a lista
+print(min(lista)) #encontra o menor valor
+print(max(lista)) #encontra o maior valor
+
+lista.append(7) #adiciona um valor à lista
+print(lista)
+
+lista.pop(-1) #remove um valor da lista
+print(lista)
+
+lista.insert(4, 4.5)
+print(lista)
+
+print(17 in lista) #verifica se existe o número na lista
+
+#matriz
+l3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+print(l3[1][2])
+
+bebidas = []
+for i in range(3):
+    bebidas.append(input(f"qual sua bebida n{i}: "))
+for b in bebidas:
+    print(b)
---
+++ 03.estruturas_de_dados/aula09_Tuplas.py
@@ -0,0 +1,7 @@
+#parecidas cons as listas, mas elas são constantes
+tu = (1, 1, 2, 1, 4, 5)
+
+print(tu.count(1)) #conta quantas vezes o 1 aparece
+
+l = list(tu) #transforma uma tuple eu uma lista
+print(l)
